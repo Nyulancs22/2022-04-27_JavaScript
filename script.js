1b2f1d34f67a7b6c0a752753ecb047687bba9ba0
Nyulancs22 <93988399+Nyulancs22@users.noreply.github.com>
2022-04-27 14:26:35 +0200

    igen

---
+++ script.js
@@ -0,0 +1,18 @@
+let tesztgomb = document.getElementById('teszt');
+
+tesztgomb.addEventListener('click', abszolut);
+
+function abszolut(){
+    let szam = prompt('Adj meg egy egész számot: ')
+
+    if (szam >= 0){
+        document.getElementById('eredmeny').innerHTML = szam
+
+    }else{
+        document.getElementById('eredmeny').innerHTML = -szam
+
+    }
+
+
+}
+
